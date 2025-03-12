fdf14e65442d0a6ee522c93b0446c3be3a23bc65
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-12 11:17:45 +0900

    section2-17 자바스크립트 변수 사용

--- section2_refresh_javascript/13_start_project/assets/scripts/app.js
+++ section2_refresh_javascript/13_start_project/assets/scripts/app.js
@@ -1,7 +1,12 @@
 // import { apiKey } from "./util.js";
 
 // import apiKey from "./util.js";
-import {apiKey, abc as content} from "./util.js";
+// import {apiKey, abc as content} from "./util.js";
 // import * as util from "./util.js";
 // console.log(util.apiKey);
-console.log(content);
+// console.log(content);
+
+let userMessage = "Hello World!!!";
+
+console.log(userMessage);
+console.log(userMessage);
