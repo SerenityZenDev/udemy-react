73543bf9f837dbf110b079f677f2aeed5e101546
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-12 10:57:03 +0900

    section2-16 multi import/export

--- section2_refresh_javascript/13_start_project/assets/scripts/util.js
+++ section2_refresh_javascript/13_start_project/assets/scripts/util.js
@@ -1,3 +1,5 @@
 // export let apiKey = "adnasdoasflak1";
-export default "adnasdoasflak1";
 // export default는 하나만 존재할 수 있다.
+export default "adnasdoasflak1";
+export let apiKey = "adnasdoasflak1"
+export let abc = "abc";
