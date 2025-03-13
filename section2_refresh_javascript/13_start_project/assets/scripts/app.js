2c5803d004976aff37042ec9572dd2fc4c9b9664
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-13 11:33:06 +0900

    화살표 함수

--- section2_refresh_javascript/13_start_project/assets/scripts/app.js
+++ section2_refresh_javascript/13_start_project/assets/scripts/app.js
@@ -15,14 +15,19 @@
 
 // console.log(10 === 5); // false | 둘이 같은가
 
-function greetUser(userName, message = "Hello") { // 함수 생성
-    console.log("Hello!");
-    console.log(userName);
-    console.log(message);
+// function greetUser(userName, message = "Hello") { // 함수 생성
+//     console.log("Hello!");
+//     console.log(userName);
+//     console.log(message);
 
-    return "Hi I am " + userName + "." + message
+//     return "Hi I am " + userName + "." + message
+// }
+
+// console.log(greetUser("Max")); // 함수 호출
+// const greeting2 = greetUser("Manuel", "Hello what's up"); // 함수 호출
+// console.log(greeting2);
+
+export default (userName, message) => {
+    console.log('Hello');
+    return userName + message;
 }
-
-console.log(greetUser("Max")); // 함수 호출
-const greeting2 = greetUser("Manuel", "Hello what's up"); // 함수 호출
-console.log(greeting2);
