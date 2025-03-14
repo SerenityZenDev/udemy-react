e1c076d24c407b10ca1f3f4ce6becb0443ade83a
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-14 10:33:33 +0900

    section2-30 함수 내부에서 함수 정의하기

--- section2_refresh_javascript/13_start_project/assets/scripts/app.js
+++ section2_refresh_javascript/13_start_project/assets/scripts/app.js
@@ -145,24 +145,34 @@
 // const list  = document.querySelector("ul");
 // list.remove();
 
-function handleTimeout(){
-    console.log("Timed out!");
+// function handleTimeout(){
+//     console.log("Timed out!");
+// }
+
+
+// const handleTimeout2 = () => {
+//     console.log("Timed out ... again");
+// };
+
+
+// setTimeout(handleTimeout, 2000);
+// setTimeout(handleTimeout2, 3000);
+// setTimeout(() => {
+//     console.log("More timing out...");
+// }, 4000);
+
+// function greeter(greetFn){
+//     greetFn();
+// }
+
+// greeter(() => console.log("Hi"));
+
+function init(){
+    function greet(){
+        console.log('Hi');
+    }
+
+    greet();
 }
 
-
-const handleTimeout2 = () => {
-    console.log("Timed out ... again");
-};
-
-
-setTimeout(handleTimeout, 2000);
-setTimeout(handleTimeout2, 3000);
-setTimeout(() => {
-    console.log("More timing out...");
-}, 4000);
-
-function greeter(greetFn){
-    greetFn();
-}
-
-greeter(() => console.log("Hi"));
+init();
