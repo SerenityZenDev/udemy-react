76100cad991b423b6595de423e89ad8bf5eff2ec
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-14 10:29:13 +0900

    section2-29 함수를 값으로 사용하기

--- section2_refresh_javascript/13_start_project/assets/scripts/app.js
+++ section2_refresh_javascript/13_start_project/assets/scripts/app.js
@@ -142,5 +142,27 @@
 //     console.log(hobby);
 // }
 
-const list  = document.querySelector("ul");
-list.remove();
+// const list  = document.querySelector("ul");
+// list.remove();
+
+function handleTimeout(){
+    console.log("Timed out!");
+}
+
+
+const handleTimeout2 = () => {
+    console.log("Timed out ... again");
+};
+
+
+setTimeout(handleTimeout, 2000);
+setTimeout(handleTimeout2, 3000);
+setTimeout(() => {
+    console.log("More timing out...");
+}, 4000);
+
+function greeter(greetFn){
+    greetFn();
+}
+
+greeter(() => console.log("Hi"));
