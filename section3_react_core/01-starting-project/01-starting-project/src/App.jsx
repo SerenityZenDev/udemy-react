df83fcd4d3ed1fa777191d4be2a9829e2dc5c64e
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-17 11:41:51 +0900

    section3-54 state 관리 & hook 사용법

--- section3_react_core/01-starting-project/01-starting-project/src/App.jsx
+++ section3_react_core/01-starting-project/01-starting-project/src/App.jsx
@@ -1,13 +1,18 @@
+import { useState } from 'react';
+
 import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+
   let tabContent = 'Please click a button';
 
   function handleSelect(selectedButton) {
-    console.log(selectedButton);
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
   }
 
   return (
@@ -35,7 +40,7 @@ function App() {
           <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
           <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
         </menu>
-        {tabContent}
+        {selectedTopic}
         </section>
       </main>
     </div>
