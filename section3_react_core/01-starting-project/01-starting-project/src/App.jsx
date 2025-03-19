2617d10b36b12bb22a37359856d80a5c16b2481d
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-19 11:14:34 +0900

    section4-64 Feature 및 State로 컴포넌트 분리하기

--- section3_react_core/01-starting-project/01-starting-project/src/App.jsx
+++ section3_react_core/01-starting-project/01-starting-project/src/App.jsx
@@ -1,34 +1,12 @@
-import { Fragment, useState } from 'react';
 
-import { CORE_CONCEPTS } from './data';
 import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-import { EXAMPLES } from './data';
+import CoreConcepts from './components/CoreConcepts.jsx';
+import Examples from './components/Examples.jsx';
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState();
+  
 
-  function handleSelect(selectedButton) {
-    setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
-  }
-
-  let tabContent = <p>Please select a topic.</p>;
-
-  if(selectedTopic) {
-    tabContent = (
-    <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
-    )
-  }
+  
   // return문 내부에 들어오는 값을 div 로 묶어줘야 함
   // div가 불필요하게 생기는게 싫다면 fragment를 사용
   // 다만 최신버전에서는 <> </> 로 대체 가능
@@ -36,25 +14,8 @@ function App() {
     <>
       <Header/>
       <main>
-        <section id='core-concepts'>
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.title}{...conceptItem}/>
-            ))}
-        
-          </ul>
-        </section>
-        <section id='examples'>
-        <h2>Examples</h2>
-        <menu>
-          <TabButton isSelected={selectedTopic === 'components'} onSelect={() => handleSelect('components')}>Components</TabButton>
-          <TabButton isSelected={selectedTopic === 'jsx'} onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-          <TabButton isSelected={selectedTopic === 'props'} onSelect={() => handleSelect('props')}>Props</TabButton>
-          <TabButton isSelected={selectedTopic === 'state'} onSelect={() => handleSelect('state')}>State</TabButton>
-        </menu>
-        {tabContent}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
     </>
   );
