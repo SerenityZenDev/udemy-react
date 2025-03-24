578bee776fa141079e7727e6cbceda8d6978235b
SerenityZenDev <tjrqo2000@gmail.com>
2025-03-24 17:51:49 +0900

    section4-94 State를 끌러올리면 안되는 이유

--- section4_react_core_advanced/07-tic-tac-toe-starting-project/src/App.jsx
+++ section4_react_core_advanced/07-tic-tac-toe-starting-project/src/App.jsx
@@ -22,6 +22,10 @@ function deriveActivePlayer(gameTurns) {
 }
 
 function App() {
+  const [players, setPlayers] = useState({
+    X: 'Player 1',
+    O: 'Player 2',
+  });
   const [gameTurns, setGameTurns] = useState([]);
   // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
@@ -66,6 +70,15 @@ function App() {
 
   function handleRestart() {
     setGameTurns([]);
+  }
+
+  function handlePlayerNameChange(symbol, newName){
+    setPlayers(prevPlayers => {
+      return {
+        ...prevPlayers,
+        [symbol]: newName
+      };
+    });
   }
 
 
