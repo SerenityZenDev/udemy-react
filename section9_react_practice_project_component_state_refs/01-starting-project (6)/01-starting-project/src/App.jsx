40bd26c89d73c58432cd92b7ef4837e32d712f55
SerenityZenDev <tjrqo2000@gmail.com>
2025-07-08 15:57:56 +0900

    section9-148 테일윈드 CSS로 사이드바 & 버튼 스타일링 하기

--- section9_react_practice_project_component_state_refs/01-starting-project (6)/01-starting-project/src/App.jsx
+++ section9_react_practice_project_component_state_refs/01-starting-project (6)/01-starting-project/src/App.jsx
@@ -2,7 +2,7 @@ import ProjectSidebar from "./components/projectSidebar";
 
 function App() {
   return (
-    <main>
+    <main className="h-screen my-8">
       <ProjectSidebar />
     </main>
   );
