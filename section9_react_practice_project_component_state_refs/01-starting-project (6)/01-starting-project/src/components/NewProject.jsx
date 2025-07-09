2145eea8e5f013c087cff8b1b21b0de4a767b94d
SerenityZenDev <tjrqo2000@gmail.com>
2025-07-09 16:24:13 +0900

    section9-155 사용자 입력 유효성 검사 & useImperativeHandle로 에러 모달 띄우기

--- section9_react_practice_project_component_state_refs/01-starting-project (6)/01-starting-project/src/components/NewProject.jsx
+++ section9_react_practice_project_component_state_refs/01-starting-project (6)/01-starting-project/src/components/NewProject.jsx
@@ -1,15 +1,25 @@
 import { useRef } from "react";
+
 import Input from "./Input"
+import Modal from "./Modal";
 
 export default function NewProject({onAdd}) {
+    const modal = useRef();
+
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
+
 
     function handleSave(){
         const enteredTitle = title.current.value;
         const enteredDescription = title.current.value;
         const enteredDueDate = title.current.value;    
+
+        if(enteredTitle.trim() === '' || enteredDescription.trim() === '', enteredDueDate.trim() === ''){
+        modal.current.open();
+        return;
+    }
         
         onAdd({
             title: enteredTitle,
@@ -19,6 +29,12 @@ export default function NewProject({onAdd}) {
     }
 
     return (
+    <>
+    <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Input</h2>
+        <p>Oops ... looks like you forgot to enter a value</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+    </Modal>
     <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
@@ -34,5 +50,6 @@ export default function NewProject({onAdd}) {
             <Input type="date" ref={dueDate} label="Due Date"/>
         </div>
     </div>
+    </>
     );
 }
