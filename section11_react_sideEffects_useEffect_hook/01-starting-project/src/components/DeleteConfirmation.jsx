d73b5127010c7ffcf5da51d3fbabc014eb5a3cf9
SerenityZenDev <tjrqo2000@gmail.com>
2025-07-11 17:05:15 +0900

    section11-190 State 업데이트 최적화

--- section11_react_sideEffects_useEffect_hook/01-starting-project/src/components/DeleteConfirmation.jsx
+++ section11_react_sideEffects_useEffect_hook/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,16 +1,9 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
+import ProgressBar from './ProgressBar';
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({ onConfirm, onCancel }) {
-  const TIMER = 3000;
-  const [remainingTime, setRemainingTime] = useState(TIMER);
-
-
-  useEffect(() => {
-    setInterval(() => {
-      setRemainingTime(prevTime => prevTime - 10);
-    }, 10);
-  }, []);
-
 
   useEffect(() => {
     console.log('TIMER SET');
@@ -37,7 +30,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
-      <progress value={remainingTime} max={TIMER} />
+      <ProgressBar timer={TIMER} />
     </div>
   );
 }
